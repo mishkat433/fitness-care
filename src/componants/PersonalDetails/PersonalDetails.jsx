0472a253ec90c247e0f00930d5279f0a569b03be
miskat433 <miskat433@gmail.com>
2022-09-29 12:15:20 +0600

    complite

--- src/componants/PersonalDetails/PersonalDetails.jsx
+++ src/componants/PersonalDetails/PersonalDetails.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import avatar from '../../images/Mishkat pp size.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faSwimmer, faHiking, faDisplay } from '@fortawesome/free-solid-svg-icons';
 
 const PersonalDetails = () => {
+
     return (
         <div>
             <div className="flex gap-3 items-center">
@@ -14,18 +15,19 @@ const PersonalDetails = () => {
                     <p className='text-gray-500'><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon> Cox's Bazar</p>
                 </div>
             </div>
-            <div className='bg-orange-100 mt-5 flex p-2 lg:p-4 justify-between rounded-xl'>
+            <h4 className='text-xl lg:text-2xl mt-2 font-semibold'>Hobbies</h4>
+            <div className='bg-orange-100 mt-1 flex p-2 lg:p-4 justify-between rounded-xl'>
                 <div className=''>
-                    <p><span className='text-2xl lg:text-3xl font-semibold'>68</span>kg</p>
-                    <p>Weight</p>
+                    <p className='text-5xl text-center text-orange-700'><FontAwesomeIcon icon={faSwimmer}></FontAwesomeIcon></p>
+                    <p>Swimming</p>
                 </div>
                 <div className=''>
-                    <p><span className='text-2xl lg:text-3xl font-semibold'>5.7</span>in</p>
-                    <p>Height</p>
+                    <p className='text-5xl text-center text-orange-700'><FontAwesomeIcon icon={faHiking}></FontAwesomeIcon></p>
+                    <p>Hiking</p>
                 </div>
                 <div className=''>
-                    <p><span className='text-2xl lg:text-3xl font-semibold'>19</span>yrs</p>
-                    <p>Age</p>
+                    <p className='text-5xl text-center text-orange-700'><FontAwesomeIcon icon={faDisplay}></FontAwesomeIcon></p>
+                    <p>Codding</p>
                 </div>
             </div>
 
--- src/componants/Sidebar/Sidebar.jsx
+++ src/componants/Sidebar/Sidebar.jsx
@@ -28,9 +28,9 @@ const Sidebar = ({ time, setTime }) => {
     const breakTimeArray = [10, 20, 30, 40, 50];
 
     return (
-        <div className='mt-8'>
-            <h1 className='text-xl lg:text-2xl font-semibold'>Add A Break</h1>
-            <div className='bg-orange-100 text-white mt-5 flex p-4 justify-between items-center rounded-xl text-xl'>
+        <div className='mt-6'>
+            <h1 className='text-xl lg:text-2xl font-semibold'>Add A Break :</h1>
+            <div className='bg-orange-100 text-white mt-2 flex p-4 justify-between items-center rounded-xl text-xl'>
 
                 {
                     breakTimeArray.map(second => <p key={second} className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(second)}><span>{second}</span>s</p>)
@@ -38,8 +38,8 @@ const Sidebar = ({ time, setTime }) => {
 
             </div>
 
-            <h1 className='text-xl lg:text-2xl font-semibold mt-8'>Exercise Details</h1>
-            <div className='bg-orange-100 flex justify-between items-center p-4 text-md lg:text-xl rounded-xl my-6' >
+            <h1 className='text-xl lg:text-2xl font-semibold mt-5'>Exercise Details :</h1>
+            <div className='bg-orange-100 flex justify-between items-center p-4 text-md lg:text-xl rounded-xl my-4' >
                 <h5 > Exercise Time: </h5 >
                 <h5>{time} seconds</h5>
             </div>
