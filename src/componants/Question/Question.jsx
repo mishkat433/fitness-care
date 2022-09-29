ddbcd356c1abeaa4dadbf1fb1e549ae450ee7bdb
miskat433 <miskat433@gmail.com>
2022-09-29 11:43:41 +0600

    sum change

--- src/componants/Question/Question.jsx
+++ src/componants/Question/Question.jsx
@@ -20,12 +20,12 @@ const Question = () => {
                     <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components</p>
                 </div>
             </div>
-            <div tabIndex={1} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+            <div tabIndex={2} className="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
                 <div className="collapse-title text-xl font-medium">
                     The useEffect is used for some other purpose than loading data?
                 </div>
                 <div className="collapse-content">
-                    <p>We declare the count state variable, and then we tell React we need to use an effect. We pass a function to the useEffect Hook. This function we pass is our effect. Inside our effect, we set the document title using the document.title browser API. We can read the latest count inside the effect because it’s in the scope of our function. When React renders our component, it will remember the effect we used, and then run our effect after updating the DOM. This happens for every render, including the first one.</p>
+                    <p>We declare the count state variable, and then we tell React we need to use an effect. We pass a function to the useEffect Hook. This function we pass is our effect. Inside our effect, we set the document title using the document.title browser API. We can read the latest count inside the effect because it's in the scope of our function. When React renders our component, it will remember the effect we used, and then run our effect after updating the DOM. This happens for every render, including the first one.</p>
                 </div>
             </div>
         </div>
--- src/componants/Sidebar/Sidebar.jsx
+++ src/componants/Sidebar/Sidebar.jsx
@@ -16,25 +16,28 @@ const Sidebar = ({ time, setTime }) => {
         }
     }
 
-    const notify = () => {
+    const toastify = () => {
         toast("Your activity successfully completed");
 
         localStorage.setItem("break-time", 0);
         setBreaktime(0);
 
-        setTime(0)
+        setTime(0);
     }
+
+    const breakTimeArray = [10, 20, 30, 40, 50];
 
     return (
         <div className='mt-8'>
             <h1 className='text-xl lg:text-2xl font-semibold'>Add A Break</h1>
             <div className='bg-orange-100 text-white mt-5 flex p-4 justify-between items-center rounded-xl text-xl'>
-                <p className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(10)}><span>10</span>s</p>
-                <p className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(20)}><span>20</span>s</p>
-                <p className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(30)}><span>30</span>s</p>
-                <p className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(40)}><span>40</span>s</p>
-                <p className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(50)}><span>50</span>s</p>
+
+                {
+                    breakTimeArray.map(second => <p key={second} className='bg-orange-700 rounded-2xl p-2 w-14 h-14 items-center flex justify-center hover:bg-orange-900 cursor-pointer' onClick={() => breakHandle(second)}><span>{second}</span>s</p>)
+                }
+
             </div>
+
             <h1 className='text-xl lg:text-2xl font-semibold mt-8'>Exercise Details</h1>
             <div className='bg-orange-100 flex justify-between items-center p-4 text-md lg:text-xl rounded-xl my-6' >
                 <h5 > Exercise Time: </h5 >
@@ -44,7 +47,7 @@ const Sidebar = ({ time, setTime }) => {
                 <h5>Break Time : </h5>
                 <h5>{breaktime ? breaktime : localStorage.getItem("break-time")} seconds</h5>
             </div>
-            <button className="btn btn-active bg-orange-700 mt-10 w-full text-md lg:text-lg" onClick={notify}>Activity Completed</button>
+            <button className="btn btn-active bg-orange-700 mt-10 w-full text-md lg:text-lg" onClick={toastify}>Activity Completed</button>
             <ToastContainer />
         </div >
     );
